456447cfabc7cf399e6c4845d729a922d577c6d5
NanodAaa <102843392+NanodAaa@users.noreply.github.com>
2023-11-18 14:14:22 +0100

    Update DS1302.h

--- Electrical Clock/DS1302.h
+++ Electrical Clock/DS1302.h
@@ -1,7 +1,4 @@
-void ds1302_write_data(unsigned char cmd, unsigned char dat); //向DS1302写入数据
-
-unsigned char ds1302_read(unsigned char cmd); //从DS1302读出数据
-
-unsigned char dat_to_BCD(unsigned char dat); //转码
-
-unsigned char BCD_to_dat(unsigned char dat); //转码
+void ds1302_write_data(unsigned char cmd, unsigned char dat); //鍚慏S1302鍐欏叆鏁版嵁
+unsigned char ds1302_read(unsigned char cmd); //浠嶥S1302璇诲嚭鏁版嵁
+unsigned char dat_to_BCD(unsigned char dat); //杞爜
+unsigned char BCD_to_dat(unsigned char dat); //杞爜
